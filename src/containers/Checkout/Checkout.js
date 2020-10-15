6c866377b6e0d572256ac09a0faf889e7c70eba2
Alex Ho <huyho114@gmail.com>
2020-10-15 15:01:27 -0400

    add routing, checkout, order and customerdata

---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import { Route } from 'react-router-dom';
+import ContactData from './ContactData/ContactData'
+
+class Checkout extends Component {
+    state = {
+        ingredients:{
+            salad:0,
+            meat: 0,
+            cheese:0,
+            bacon:0
+        },
+        totalPrice:0
+    }
+    componentDidMount(){
+        const query = new URLSearchParams(this.props.location.search);
+        const ingredients = {};
+        let price = 0;
+
+        for(let param of query.entries()){
+            if (param[0] === 'price') {
+                price = param[1];
+            }
+            else{
+                ingredients[param[0]] = +param[1];
+
+            }
+        }
+        this.setState({
+            totalPrice: price,
+            ingredients:ingredients
+        });
+
+    }
+    checkoutCancelledHandler=()=>{
+        this.props.history.goBack()
+    }
+    checkoutContinuedHandler=()=>{
+        this.props.history.replace('/checkout/contact-data');
+
+    }
+render() {
+    return (
+         <div>
+             <CheckoutSummary ingredients={this.state.ingredients}
+             checkoutCancelled={this.checkoutCancelledHandler}
+             checkoutContinued={this.checkoutContinuedHandler}/>
+             <Route path={this.props.match.path +'/contact-data'} render={(props)=>(<ContactData ingredients ={this.state.ingredients} totalPrice = {this.state.totalPrice} {...props}/>)}/>
+         </div>
+    );
+}
+
+}
+export default Checkout;
