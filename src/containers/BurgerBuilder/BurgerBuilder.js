356d0d1aafd66e17a23ae2f52b48ac8562f7dde2
Alex Ho <huyho114@gmail.com>
2021-02-07 23:40:23 -0500

    add basic testing using jest and enzyme

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,7 +11,7 @@ import * as actions from '../../store/actions/index';
 import axios from '../../axios-orders';
 
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
 
     state = {
         // ingredients: null,
